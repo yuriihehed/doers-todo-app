43a4eb2c81b0c06f27f5800b52a06cc9b4b715e6
Gulbanu Madiyarova <gmadiyar@depaul.edu>
2025-01-10 12:44:33 -0600

    chore: Set up Docker with PostgreSQL support and update requirements
    - Added psycopg2-binary to requirements.txt for PostgreSQL support
    - Updated Dockerfile to ensure dependencies are installed correctly
    - Verified the Docker setup by running docker-compose up --build

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
