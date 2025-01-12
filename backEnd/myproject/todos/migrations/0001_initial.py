2777c4ec782fc09d31efcc812d0e432df89b7913
Yurii Hehediush <130924406+yuriihehed@users.noreply.github.com>
2025-01-12 13:27:12 -0600

    Update 0001_initial.py

--- backEnd/myproject/todos/migrations/0001_initial.py
+++ backEnd/myproject/todos/migrations/0001_initial.py
@@ -25,4 +25,4 @@ class Migration(migrations.Migration):
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
-    ]
+    ]  # Close the operations list here
