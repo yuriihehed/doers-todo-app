8552ff9acc64af466370abb15b26c2435bb53e48
Vanessa Wartemberg <wartembergvanessa@yahoo.com>
2025-01-12 13:17:18 -0600

    some updates to the register and settings file

--- backEnd/myproject/myproject/settings.py
+++ backEnd/myproject/myproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'team3database',
         'USER': 'team3user',
-        'PASSWORD': 'password',
+        'PASSWORD': 'vanessaw',
         'HOST': 'localhost',
         'PORT': '5432',
     }
