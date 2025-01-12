7d430cfcfa507f700ad6af7ec0c3b161a64198cf
Vanessa Wartemberg <wartembergvanessa@yahoo.com>
2025-01-12 11:48:51 -0600

    added register page

--- backEnd/myproject/todos/urls.py
+++ backEnd/myproject/todos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.home, name='dashboard'),
+    path('register/', views.register, name='register'), # for the register page 
 ]
--- backEnd/myproject/todos/views.py
+++ backEnd/myproject/todos/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
     return render(request, 'dashboard.html')
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm')
+
+        # Validate the form inputs
+        if not email or not password or not confirm_password:
+            messages.error(request, 'All fields are required.')
+        elif password != confirm_password:
+            messages.error(request, 'Passwords do not match.')
+        else:
+            # user registration (e.g., save to the database)
+            messages.success(request, 'You have registered successfully!')
+            return HttpResponseRedirect('/register/')
+
+    # Render the registration template
+    return render(request, 'register.html')
