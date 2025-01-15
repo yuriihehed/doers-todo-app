2f5886ef7e604084cce1cdbe2f3cf7b39e3123c8
Gulbanu Madiyarova <gmadiyar@depaul.edu>
2025-01-14 23:40:51 -0600

    todoPage changes

--- backEnd/myproject/todos/urls.py
+++ backEnd/myproject/todos/urls.py
@@ -29,7 +29,9 @@ urlpatterns = [
     path('teams/id/', views.teams_id, name='teams_id'),
     path('accounts/', include('django.contrib.auth.urls')),  # Add this line if not already present
 
-    path('new/', views.create_todo, name='create_todo'),
-
+    path('todos/new/', views.create_todo, name='create_todo'),  # Create ToDo page
+    path('todos/<int:todo_id>/update/<str:state>/', views.update_todo_state, name='update_todo_state'),  # Update state
+    path('todos/<int:todo_id>/delete/', views.delete_todo, name='delete_todo'),  # Delete ToDo
+    path('teams/', views.teams_id, name='teams_id'),  # Teams dropdown
 ]
 
